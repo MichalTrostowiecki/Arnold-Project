34043f7f70c60c8e9f7d8ed31f3654e90536c5eb
michal.trostowiecki@gmail.com <michal.trostowiecki@gmail.com>
2023-03-03 06:45:01 +0000

    Add initial template for react based on create.sh

---
+++ arnold-project/create.sh
@@ -0,0 +1,26 @@
+echo "Creating first template"
+npx create-react-app arnold-project
+
+cd arnold-project
+cd src
+mkdir assets
+mkdir backend
+mkdir frontend
+cd frontend
+mkdir assets
+mkdir components
+mkdir layouts
+mkdir pages
+cd layouts
+mkdir footer
+mkdir header
+mkdir main
+cd footer
+touch Footer.js
+cd ..
+cd header
+touch Header.js
+cd ..
+cd main 
+touch Main.js
+cd ..
---
+++ arnold-project/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='root'>
+    	<h1>
+		Welcome to Arnold Project</h1>
+	</div>
+  );
+}
+
+export default App;
