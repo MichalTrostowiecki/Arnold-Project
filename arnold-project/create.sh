8dd014c8ccdbf3e847e9f7b4a0c72a7a5c068f8c
michal.trostowiecki@gmail.com <michal.trostowiecki@gmail.com>
2023-03-12 17:33:03 +0000

    mkdir -p is used to create all subfolders

--- arnold-project/create.sh
+++ arnold-project/create.sh
@@ -2,25 +2,20 @@ echo "Creating first template"
 npx create-react-app arnold-project
 
 cd arnold-project
-cd src
-mkdir assets
-mkdir backend
-mkdir frontend
-cd frontend
-mkdir assets
-mkdir components
-mkdir layouts
-mkdir pages
-cd layouts
-mkdir footer
-mkdir header
-mkdir main
-cd footer
-touch Footer.js
-cd ..
-cd header
-touch Header.js
-cd ..
-cd main 
-touch Main.js
-cd ..
+mkdir -p src/assets
+mkdir -p src/backend
+mkdir -p src/frontend/assets
+mkdir -p src/frontend/components
+mkdir -p src/frontend/layouts/footer
+mkdir -p src/frontend/layouts/header
+mkdir -p src/frontend/layouts/main
+mkdir -p src/frontend/pages
+touch src/frontend/layouts/footer/Footer.js
+touch src/frontend/layouts/header/Header.js
+touch src/frontend/layouts/main/Main.js
+
+
+
+
+
+
