fbde22fd78efdd3107ba47c37c7a4e954a0d9cad
MichalTrostowiecki <michal.trostowiecki@gmail.com>
2023-06-13 08:01:10 +0100

    Google Login implemented

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import App from "./App";
 
-import App from './App';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    //Line below make sure that whole React app can access the  Google Auth Provider once.
+    <GoogleOAuthProvider clientId="792823518213-lfoaflh4r36ksinfn5r07294ces1fs0c.apps.googleusercontent.com">
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </GoogleOAuthProvider>
 );
-
